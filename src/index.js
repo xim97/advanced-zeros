c2e6ffbc0f7b606838f7aaaad7cec931d705164d
Maxim Yefimovich <xim97@mail.ru>
2018-04-07 23:40:30 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+ 	var ans = 1000000000000,
+  		powers = [],
+  		copy = base,
+  		p, sum, k;  		
+  	for (var i = 2; i <= base; i++){
+  		p = 0;
+  		while (base % i === 0 && base !== 0){
+  			p++;
+  			base /= i;	
+  		}
+  		if (p !== 0){
+  			powers[i] = p;
+  		}
+  		if (base === 0){
+  			break;
+  		}
+  	}  	
+  	for (var j = 2; j <= copy; j++){
+  		if (powers[j] !== undefined){
+  			sum = 0;
+            k = number;
+            while (k / j > 0){
+                   sum += Math.trunc(k / j);
+                   k /= j;
+            }
+            ans = Math.min(ans, Math.trunc(sum / powers[j]));
+  		}
+  	}  	
+  	return ans;
 }
